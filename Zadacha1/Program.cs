1b571f01aead43ed4004ef1d1f989e5470d503c6
Nastia <titova_anastasia@tut.by>
2023-02-24 19:14:55 +0300

    Zadacha1

---
+++ Zadacha1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее
+
+Console.WriteLine("Введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 > number2)
+{
+   Console.WriteLine(number1 + ">" + number2); 
+}
+else
+{
+    Console.WriteLine(number1 + "<" + number2);
+}
