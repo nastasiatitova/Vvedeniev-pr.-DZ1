dafe8124d3e180a0769dc4b345478676d4b21cc3
Nastia <titova_anastasia@tut.by>
2023-02-27 22:59:16 +0300

    Zadacha13

---
+++ Zadacha13/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+string str = number.ToString();
+
+if (number>99)
+{
+    Console.WriteLine("Третяя цифра числа - " + str[2]);
+}
+else Console.WriteLine("Третьей цифры нет");
