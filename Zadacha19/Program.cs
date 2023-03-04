c164d7c6cdcb446b5176f65b2d6a3a4bd4b2816e
Nastia <titova_anastasia@tut.by>
2023-03-05 01:10:13 +0300

    zadacha19

---
+++ Zadacha19/Program.cs
@@ -0,0 +1,23 @@
+﻿//Напишите программу, которая принимает на вход число и проверяет, является ли оно палиндромом.
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int i = 0;
+int newnum = 0;
+int ostatok = 0;
+
+Console.WriteLine("Перевенутое число ");
+for (i = number; i > 0; i = i / 10)
+{
+    ostatok = i % 10;
+    Console.Write(ostatok);
+    newnum = newnum *10 + ostatok;
+}
+Console.WriteLine();
+if (newnum == number)
+{
+    Console.WriteLine("Число полиндром");
+}
+else Console.WriteLine("Число не полиндром");
+
