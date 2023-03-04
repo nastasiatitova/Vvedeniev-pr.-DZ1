c0cb857384404ee8262a733765dcfc69a1d89d35
Nastia <titova_anastasia@tut.by>
2023-03-04 23:59:31 +0300

    zadacha23

---
+++ Zadacha23/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= number; i++)
+
+Console.Write(i*i*i + " ");
